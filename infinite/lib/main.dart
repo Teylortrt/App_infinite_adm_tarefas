adb26c548915113d1d602ae781bb6ef162428793
Eliel Oliveira Rodrigues <159914428+Eliel567@users.noreply.github.com>
2024-09-26 18:03:01 -0300

    Update main.dart
    ta top

--- infinite/lib/main.dart
+++ infinite/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:infinite/screens/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,29 +13,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFF1980BA),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-  const Text(
-    'infinite',
-    style: TextStyle(
-      fontSize: 50,
-      fontWeight: FontWeight.bold,
-    ),
-  ),
-  const SizedBox(width: 5),
-  Image.asset(
-    'assets/infinite.png',
-    width: 100,
-    height:50,
-  ),
-],
+        body: GestureDetector(
+          onTap: () {
+            // Navegar para a tela Home ao clicar
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const TelaInicial()),
+            );
+          },
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'infinite',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Image.asset(
+                  'assets/infinite.png',
+                  width: 100,
+                  height: 50,
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
